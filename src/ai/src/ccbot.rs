c684da590f628be41ec58da9a2d9c175c541a456
asrcpq <asrcpq@outlook.com>
2021-03-12 11:25:22 +0000

    bug fix

--- src/ai/src/ccbot.rs
+++ src/ai/src/ccbot.rs
@@ -9,7 +9,7 @@ use std::collections::VecDeque;
 
 pub struct CCBot {
 	interface: Interface,
-	first_preview: bool,
+	preview_list: [u8; 6],
 }
 
 fn map_key(pm: PieceMovement) -> KeyType {
@@ -49,7 +49,7 @@ impl Default for CCBot {
 		let interface = get_if();
 		CCBot {
 			interface,
-			first_preview: true,
+			preview_list: [7u8; 6],
 		}
 	}
 }
@@ -71,21 +71,12 @@ fn code_to_piece(code: u8) -> libtetris::Piece {
 impl Thinker for CCBot {
 	fn reset(&mut self) {
 		self.interface = get_if();
-		self.first_preview = true;
+		self.preview_list = [7u8; 6];
 	}
 
-	// currently we do not deal with garbages
+	// TODO: handle garbages
 	fn main_think(&mut self, display: Display) -> VecDeque<KeyType> {
-		if self.first_preview {
-			// feed previews
-			self.interface.add_next_piece(code_to_piece(display.floating_block[2]));
-			for &code in display.bag_preview.iter() {
-				self.interface.add_next_piece(code_to_piece(code));
-			}
-			self.first_preview = false;
-		} else {
-			self.interface.add_next_piece(code_to_piece(display.bag_preview[5]));
-		}
+		self.update_preview(&display.bag_preview, display.floating_block[2]);
 		self.interface.request_next_move(0);
 		match self.interface.block_next_move() {
 			None => panic!("CC returns none!"),
@@ -93,6 +84,42 @@ impl Thinker for CCBot {
 				eprintln!("{:?}", moves.inputs);
 				proc_moves(moves.hold, &moves.inputs)
 			},
+		}
+	}
+}
+
+impl CCBot {
+	fn update_preview(&mut self, new_list: &[u8; 6], current: u8) {
+		if self.preview_list[0] == 7 {
+			// feed previews
+			self.interface.add_next_piece(code_to_piece(current));
+			for &code in new_list.iter() {
+				eprintln!("add {}", code);
+				self.interface.add_next_piece(code_to_piece(code));
+			}
+			self.preview_list = *new_list;
+		} else {
+			// the head of new preview is index of last preview
+			'a: for last_pos in 0..6 {
+				let mut old_id = last_pos;
+				let mut new_pos = 0;
+				loop {
+					if self.preview_list[old_id] != new_list[new_pos] {
+						continue 'a
+					}
+					new_pos += 1;
+					old_id += 1;
+					if old_id == 6 {
+						while new_pos < 6 {
+							eprintln!("add {}", new_list[new_pos]);
+							self.interface.add_next_piece(code_to_piece(new_list[new_pos]));
+							new_pos += 1;
+						}
+						break 'a
+					}
+				}
+			}
+			self.preview_list = *new_list;
 		}
 	}
 }
