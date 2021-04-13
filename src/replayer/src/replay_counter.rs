53c14f98e5fdd28b81a1051b734982999a9302dc
asrcpq <asrcpq@outlook.com>
2021-04-13 23:44:10 +0000

    update replayer

--- src/replayer/src/replay_counter.rs
+++ src/replayer/src/replay_counter.rs
@@ -6,21 +6,25 @@ pub struct ReplayCounter {
 	atk: u32,
 	piece: u32,
 	key: u32,
+	twist: u32,
+	non_twist: u32,
+	twist_flag: bool,
 	duration: u128,
 }
 
 impl fmt::Display for ReplayCounter {
 	fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-		let a = self.atk as f64;
-		let p = self.piece as f64;
-		let k = self.key as f64;
-		let d = self.duration as f64;
+		let a = self.atk as f32;
+		let p = self.piece as f32;
+		let k = self.key as f32;
+		let d = self.duration as f32;
 		write!(
 			formatter,
-			"APP: {} KPP: {} PPS: {}",
+			"APP: {} KPP: {} PPS: {} TNT: {}",
 			a / p,
 			k / p,
-			p / d * 1e6f64,
+			p / d * 1e6f32,
+			self.twist as f32 / self.non_twist as f32,
 		)
 	}
 }
@@ -33,20 +37,30 @@ impl ReplayCounter {
 			BoardReply::PlainDrop(_) => {
 				self.piece += 1;
 				self.key += 1;
+				self.twist_flag = false;
 			}
 			BoardReply::ClearDrop(_lc, _atk, raw) => {
 				self.atk += raw;
 				self.piece += 1;
 				self.key += 1;
+				// this is not precise(consider a harddrop after twist)
+				if self.twist_flag {
+					self.twist += 1;
+					self.twist_flag = false;
+				} else {
+					self.non_twist += 1;
+				}
 			}
 			BoardReply::Ok => {
 				self.key += 1;
+				self.twist_flag = false;
 			}
 			BoardReply::BadMove => {
 				self.key += 1;
 			}
 			BoardReply::RotateTwist => {
 				self.key += 1;
+				self.twist_flag = true;
 			}
 			_ => {}
 		}
